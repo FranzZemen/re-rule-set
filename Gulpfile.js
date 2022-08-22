24b21cf13d58b25737eb549112a2fea7d6735564
Franz Zemen <franzzemen@gmail.com>
2022-08-22 08:03:51 -0400

    ncu

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,7 +1,36 @@
-
+const join = require('path').join;
 const gulpBase = require('@franzzemen/gulp-base').init(require('./package.json'), 100, true);
 require('@franzzemen/gulp-base').setMainBranch('main');
 
+
+const npmu = require('@franzzemen/npmu').npmu;
+
+exports.npmu = (cb) => npmu([
+  {
+    path: join(__dirname, '../gulp-base'), packageName: '@franzzemen/gulp-base',
+  }, {
+    path: join(__dirname, '../npmu'), packageName: '@franzzemen/npmu',
+  }, {
+    path: join(__dirname, '../app-utility'), packageName: '@franzzemen/app-utility',
+  }, {
+    path: join(__dirname, '../re-common'), packageName: '@franzzemen/re-common',
+  }, {
+    path: join(__dirname, '../re-data-type'), packageName: '@franzzemen/re-data-type',
+  }, {
+    path: join(__dirname, '../re-expression'), packageName: '@franzzemen/re-expression',
+  }, {
+    path: join(__dirname, '../re-condition'), packageName: '@franzzemen/re-condition',
+  }, {
+    path: join(__dirname, '../re-logical-condition'), packageName: '@franzzemen/re-logical-condition',
+  }, {
+    path: join(__dirname, '../re-rule'), packageName: '@franzzemen/re-rule',
+  }, {
+    path: join(__dirname, './'), packageName: '@franzzemen/re-rule-set',
+  }])
+  .then(() => {
+    console.log('cb...');
+    cb();
+  });
 
 exports.buildTest = gulpBase.buildTest;
 exports.test = gulpBase.test;
