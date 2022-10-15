e15ddfa48a01a042669931b352891d1821b7ad31
Franz Zemen <franzzemen@gmail.com>
2022-10-15 16:36:45 -0400

    scope field

--- ts-src/scope/rule-set-scope.ts
+++ ts-src/scope/rule-set-scope.ts
@@ -2,9 +2,12 @@ import {ExecutionContextI} from '@franzzemen/app-utility';
 import {Scope} from '@franzzemen/re-common';
 import {RuleScope} from '@franzzemen/re-rule';
 import {RuleSetOptions} from './rule-set-options.js';
+import {RuleSetParser} from '../parser/rule-set-parser.js';
 
 export class RuleSetScope extends RuleScope {
+  static RuleSetParser = 'RuleSetParser';
   constructor(options?: RuleSetOptions, parentScope?: Scope, ec?:ExecutionContextI) {
     super(options, parentScope, ec);
+    this.set(RuleSetScope.RuleSetParser, new RuleSetParser());
   }
 }
